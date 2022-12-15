65e7eb825a27fa1ad65cf2c22bc8449f3d05b69a
Roel Castano <roelinvesting@gmail.com>
2022-12-15 12:11:26 -0500

    Initial Commit

---
+++ CleanArchitectureComposition/CleanArchitectureCompositionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureCompositionApp.swift
+//  CleanArchitectureComposition
+//
+//  Created by Roel Castano on 12/15/22.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureCompositionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
